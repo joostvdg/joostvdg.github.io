90924993010e65037ac6af60f9f4026737f294d9
Joost van der Griendt <j.vandergriendt@flusso.nl>
2017-06-24 19:28:56 +0200

    Fine, than we force it!

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,4 +2,4 @@
 git config --global user.email ${GIT_EMAIL}
 git config --global user.name ${GIT_NAME}
 git remote add github https://${GH_TOKEN}@github.com/joostvdg/joostvdg.github.io.git
-mkdocs gh-deploy -b master -r github
+mkdocs gh-deploy --clean --remote-branch master --remote-name github --force --verbose
